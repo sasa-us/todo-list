430be453fd5a4e1eae13f4e19120c145e69777f7
sasa-us <sasa_luu@hotmail.com>
2018-10-04 15:52:26 -0700

    toggle complete class setup

--- src/components/list/item.js
+++ src/components/list/item.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Item =(props)=> {
     console.log('item props ', props);
     return (
-        <li className="collection-item row">
-            <div className="col s10">
+        <li className="collection-item row to-do-item">
+            {/* <div className="col s10 complete"> */}
+            <div className={ `col s10 ${props.complete ? 'complete' : ''}`} >
+
                 {props.title}
             </div>
             <div className="col s2 right-align">
