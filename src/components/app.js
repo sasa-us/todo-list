2d6f02f8ddd0382f761eef7f593598e7ebe16bd6
sasa-us <sasa_luu@hotmail.com>
2018-10-04 09:13:44 -0700

    get data from server add rebuild dom every time

--- src/components/app.js
+++ src/components/app.js
@@ -2,8 +2,9 @@ import 'materialize-css/dist/css/materialize.min.css'
 import React, {Component} from 'react';
 import List from './list';
 import AddItem from './add_item';
-import listData from '../data/list';
-
+// import listData from '../data/list';
+import axios from 'axios';
+import { ESRCH } from 'constants';
 class App extends Component {
     constructor(props) {
         super(props);
@@ -11,24 +12,62 @@ class App extends Component {
         this.state = {
             list: []
         };
+        this.base_url = 'http://api.reactprototypes.com';
+        // this.api_key = '?key=c318demouser';
+        this.api_key = '?key=sasa-us';
+        //pick at least 8 letters long after ?key= for my own api key 
     }
 
     componentDidMount() {
         this.getListData();
     }
 
-    getListData() {
+    async getListData() {
 
         //make call to server to get data
-        this.setState({
-            list: listData
-        });
+        //p1 use dummy data
+        // this.setState({
+        //     list: listData
+        // });
+
+        //p2 use axios 
+        // axios.get(`${this.base_url}/todos${this.api_key}`).then( res => {
+        //     console.log('get todos response', res.data.todos);
+        //     this.setState({
+        //         list: res.data.todos
+        //     });
+        // }).catch( err=> {
+        //     console.log('get todos error', err.message);
+        // });
+
+        //p3 use await need add async infornt of function name
+        try{
+            const response = await axios.get(`${this.base_url}/todos${this.api_key}`);
+
+            this.setState({
+                list: response.data.todos
+            });
+        } catch(err) {
+            console.log('data error ', err.message);
+        }
+        
+
     }
 
-    addItem(item) {
-        this.setState({
-            list: [item, ...this.state.list]
-        });
+    async addItem(item) {
+        //p1 only update list, then show on dom
+        // this.setState({
+        //     list: [item, ...this.state.list]
+        // });
+//p2 delete all old list on dom, call server pull data and show all/replace all on Dom , rebuild DOM 
+         try{
+            const response = await axios.post(`${this.base_url}/todos${this.api_key}`, item);
+            console.log(response);
+            this.getListData();
+        } catch(err) {
+             console.log('error adding item ', err.message);
+         }       
+
     }
 
     render() {
